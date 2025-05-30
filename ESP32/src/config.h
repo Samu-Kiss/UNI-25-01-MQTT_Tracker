638318f1a16aea9f7bbd9ff98208f210e04d71eb
Samuk <sammy.pico.duran@gmail.com>
2025-05-30 10:42:16 -0500

    [Feat] Conexion a WIFI

---
+++ ESP32/src/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// WiFi configuration
+#define WIFI_SSID "TU_SSID"
+#define WIFI_PASSWORD "TU_PASSWORD"
+
+// MQTT configuration
+
+#define MQTT_BROKER_ADRRESS [] = "test.mosquitto.org";  // CHANGE TO MQTT BROKER'S ADDRESS
+#define MQTT_PORT = 1883;
+#define MQTT_CLIENT_ID [] = "YOUR-NAME-esp32-001";      // CHANGE IT AS YOU DESIRE
+#define MQTT_USERNAME [] = "";                          // CHANGE IT IF REQUIRED, empty if not required
+#define MQTT_PASSWORD [] = "";                          // CHANGE IT IF REQUIRED, empty if not required
